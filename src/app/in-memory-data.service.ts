239093a2a9cdbd77165bfa1564f2e1c94618695c
selimelawwa <selimalawwa335@gmail.com>
2018-03-31 23:02:33 +0200

    after lisitng meals

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -14,10 +14,21 @@ export class InMemoryDataService implements InMemoryDbService {
     
 
       const meals = [
-        {id:1, rid: 1, name: 'Cheese Burger', price: '55 EGP', ingredients:'Burger, blue cheese sauce, onions, roasted tomatoes, American cheese'},
-        { id: 2, rid: 1, name: 'Italian Angus', price: '65 EGP', ingredients: 'Angus burger, red onion, tomatoes, lettuce, mozzarella cheese, garlic sauce, spicy Bruschetta sauce: tomato with grilled red pepper, served in brioche bread' },
-        { id: 3, rid: 1, name: 'Spicy Chicken Rapper', price: '45 EGP', ingredients: 'Spicy chicken tender, topped with American cheese, lettuce and Santa Fe sauce, served in tortilla brea' },
-        
+        {id:1, rid: 1, name: 'Cheese Burger',photo:'assets/burger.jpg', price: '55 EGP', ingredients:'Burger, blue cheese sauce, onions, roasted tomatoes, American cheese'},
+        { id: 2, rid: 1, name: 'Italian Angus Burger', photo: 'assets/burger2.jpg', price: '65 EGP', ingredients: 'Angus burger, red onion, tomatoes, lettuce, mozzarella cheese, garlic sauce, spicy Bruschetta sauce: tomato with grilled red pepper, served in brioche bread' },
+        { id: 3, rid: 1, name: 'Spicy Chicken Rapper', photo: 'assets/burger3.jpg', price: '45 EGP', ingredients: 'Spicy chicken tender, topped with American cheese, lettuce and Santa Fe sauce, served in tortilla brea' },
+        { id: 4, rid: 5, name: 'Pepperoni Pizza', photo: 'assets/pizza1.jpg', price: '85 EGP', ingredients: 'layers of beef pepperoni and mozzarella cheese' },
+        { id: 5, rid: 5, name: 'Deluxe Pizza', photo: 'assets/pizza2.jpg', price: '76 EGP', ingredients: 'beef pepperoni, Italian sausages, green peppers, mushrooms and onions' },
+        { id: 6, rid: 5, name: 'Veggie Pizza', photo: 'assets/pizza3.jpg', price: '66 EGP', ingredients: 'green peppers, onions, mushrooms, tomatoes and black olives' },
+        { id: 4, rid: 6, name: 'Pepperoni Pizza', photo: 'assets/pizza1.jpg', price: '85 EGP', ingredients: 'layers of beef pepperoni and mozzarella cheese' },
+        { id: 5, rid: 6, name: 'Deluxe Pizza', photo: 'assets/pizza2.jpg', price: '78 EGP', ingredients: 'beef pepperoni, Italian sausages, green peppers, mushrooms and onions' },
+        { id: 6, rid: 6, name: 'Veggie Pizza', photo: 'assets/pizza3.jpg', price: '87 EGP', ingredients: 'green peppers, onions, mushrooms, tomatoes and black olives' },
+        { id: 4, rid: 4, name: 'Pepperoni Pizza', photo: 'assets/pizza1.jpg', price: '85 EGP', ingredients: 'layers of beef pepperoni and mozzarella cheese' },
+        { id: 5, rid: 4, name: 'Meat lovers Pizza', photo: 'assets/pizza2.jpg', price: '76 EGP', ingredients: 'beef pepperoni, Italian sausages, green peppers, mushrooms and onions' },
+        { id: 6, rid: 4, name: 'Veggie Pizza', photo: 'assets/pizza3.jpg', price: '66 EGP', ingredients: 'green peppers, onions, mushrooms, tomatoes and black olives' },
+        { id: 1, rid: 8, name: 'Cheese Burger', photo: 'assets/burger.jpg', price: '55 EGP', ingredients: 'Burger, blue cheese sauce, onions, roasted tomatoes, American cheese' },
+        { id: 2, rid: 8, name: 'Italian Angus Burger', photo: 'assets/burger2.jpg', price: '65 EGP', ingredients: 'Angus burger, red onion, tomatoes, lettuce, mozzarella cheese, garlic sauce, spicy Bruschetta sauce: tomato with grilled red pepper, served in brioche bread' },
+        { id: 3, rid: 8, name: 'Spicy Chicken Rapper', photo: 'assets/burger3.jpg', price: '45 EGP', ingredients: 'Spicy chicken tender, topped with American cheese, lettuce and Santa Fe sauce, served in tortilla brea' },
       ]
     return { restaurants,meals };
   }
